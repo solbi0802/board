d1525220281a07f46077a1df18919983079139a4
DESKTOP-J80L466\Solbi-PC <solbi5555@gmail.com>
2018-11-06 00:57:52 +0900

    글목록 폼

---
+++ code/list.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<title>답변형 게시판</title>
+<style>
+</style>
+</head>
+<body topmargin=0 leftmargin=0 text=#464646>
+<center>
+<BR>
+<!-- 게시물 리스트를 보이기 위한 테이블 -->
+<table width=600 border=0 cellpadding=2 cellspacing=1 bgcolor=#4787c6>
+<!-- 리스트 타이틀 부분 -->
+<tr height=40 bgcolor=#4787c6>
+    <td width=40 align=center>
+        <font color=white>번호</font>
+    </td>
+    <td width=400 align=center>
+        <font color=white>제 목</font>
+    </td>
+    <td width=80 align=center>
+        <font color=white>작성자</font>
+    </td>
+    <td width=80 align=center>
+        <font color=white>작성일</font>
+    </td>
+    <td width=60 align=center>
+        <font color=white>조회수</font>
+    </td>
+</tr>
+<!-- 리스트 타이틀 끝 -->
+<!-- 리스트 부분 시작 -->
+
+<!-- 행 시작 -->
+<tr>
+    <!-- 번호 -->
+    <td height=20 bgcolor=white align=center>
+
+    </td>
+    <!-- 번호 끝 -->
+    <!-- 제목 -->
+    <td height=20 bgcolor=white>&nbsp;
+
+    </td>
+    <!-- 제목 끝 -->
+    <!-- 이름 -->
+    <td align=center height=20 bgcolor=white>
+        <font color=black>
+        </font>
+    </td>
+    <!-- 이름 끝 -->
+    <!-- 날짜 -->
+    <td align=center height=20 bgcolor=white>
+
+    </td>
+    <!-- 날짜 끝 -->
+    <!-- 조회수 -->
+    <td align=center height=20 bgcolor=white>
+    </td>
+<!-- 조회수 끝 -->
+</tr>
+<!-- 행 끝 -->
+</font>
+</td>
+</tr>
+</table>
+<a href=write.php>글쓰기</a>
+</center>
+</body>
+</html>
