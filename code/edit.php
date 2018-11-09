669b5b9476415d5dfc23230d64149cb6a7092336
DESKTOP-J80L466\Solbi-PC <solbi5555@gmail.com>
2018-11-10 00:41:55 +0900

    게시글 수정 기능 구현

---
+++ code/edit.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<title>답변형 게시판</title>
+<style>
+</style>
+</head>
+<body topmargin=0 leftmargin=0 text=#464646>
+<center>
+<BR>
+<form action=update.php?id=<?=$_GET[id]?> method=post>
+<table width=580 border=0 cellpadding=2 cellspacing=1 bgcolor=#4787c6>
+<tr>
+    <td height=20 align=center bgcolor=#4787c6>
+        <font color=white><B>글 수 정</B></font>
+    </td>
+</tr>
+<?php
+  include "db_info.php";
+  $result = mysqli_query($conn, "SELECT * FROM $board WHERE id= $_GET[id]");
+  $row = mysqli_fetch_array($result);
+?>
+<tr>
+    <td bgcolor=white>&nbsp;
+<table>
+<tr>
+    <td width=60 align=left >작성자</td>
+    <td align=left >
+        <INPUT type=text name=writer size=20 maxlength=10 value=<?=$row[writer]?>>
+    </td>
+</tr>
+<tr></tr>
+<tr>
+    <td width=70 align=left >비밀번호</td>
+    <td align=left >
+        <INPUT type=password name=pwd size=8 maxlength=8>
+    </td>
+</tr>
+<tr></tr>
+<tr>
+    <td width=60 align=left >제 목</td>
+    <td align=left >
+        <INPUT type=text name=title size=60 maxlength=35 value=<?=$row[title]?>>
+    </td>
+</tr>
+<tr></tr>
+<tr>
+    <td width=60 align=left >내용</td>
+    <td align=left >
+        <textarea name=content cols=65 rows=15><?=$row[content]?></textarea>
+    </td>
+</tr>
+<tr>
+    <td colspan=10 align=center>
+        <INPUT type=submit value="글쓰기">
+        &nbsp;&nbsp;
+        <INPUT type=reset value="초기화">
+        &nbsp;&nbsp;
+        <INPUT type=button value="뒤로" onclick="history.back(-1)">
+    </td>
+</tr>
+</TABLE>
+</td>
+</tr>
+<!-- 입력 부분 끝 -->
+</table>
+</form>
+</center>
+</body>
+</html>
---
+++ code/update.php
@@ -0,0 +1,25 @@
+<?php
+include "db_info.php";
+$id = $_GET['id'];
+$result = mysqli_query($conn, "SELECT pwd FROM $board WHERE id = $id");
+$row = mysqli_fetch_array($result);
+$pwd = $_POST['pwd'];
+$writer = $_POST['writer'];
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+if ($_POST[pwd] == $row[pwd]) {
+  $query = "UPDATE $board SET writer = '$writer',title= '$title', content= '$content'
+  WHERE id = $id";
+  $result = mysqli_query($conn, $query);
+} else {
+  echo ("<script>alert('비밀번호가 틀립니다.');
+  history.go(-1);
+  </script>");
+  exit;
+}
+  mysqli_close($conn);
+  echo ("<meta http-equiv='Refresh' content='3; URL=read.php?id=$_GET[id]'>");
+  ?>
+<center>
+<script> alert("수정이 완료되었습니다."); </script>
