f7377ab60894e42fb90342c8cb18910ef5c64c07
DESKTOP-J80L466\Solbi-PC <solbi5555@gmail.com>
2018-11-15 14:52:55 +0900

    최종 수정

--- code/list.php
+++ code/list.php
@@ -5,19 +5,17 @@ include "db_info.php";
 $page_size = 10;
 // 페이지 수
 $page_list_size = 10;
-$no = isset($_GET['no']) ? htmlspecialchars($_GET['no']) : 0;
-if (!$no || $no < 0) $no=0;
+$no = isset($_GET['no']) ? $_GET['no'] : 0;
+$search_word = isset($_GET['search_word']) ? $_GET['search_word'] : '';
+$field = isset($_GET['field']) ? $_GET['field'] : '';
 
-$search_word = isset($_GET['search_word']) ? htmlspecialchars($_GET['search_word']) : '';
-$field = isset($_GET['field']) ? htmlspecialchars($_GET['field']) : '';
-
-if ($search_word) {
+if ($search_word != "") {
     $search = " where $field like '%" . $search_word . "%' ";
 } else {
   $search = "";
 }
 
-$query = "SELECT * FROM $board".$search." ORDER BY thread DESC LIMIT $no, $page_size";
+$query = "SELECT * FROM $board ".$search." ORDER BY thread DESC LIMIT $no, $page_size";
 $result =  mysqli_query($conn, $query);
 
 // 총 게시물 수
@@ -129,7 +127,7 @@ $current_page = floor($no / $page_size);
 
   if ($start_page >= $page_list_size) {
   $prev_list = ($start_page - 1)* $page_size;
-  echo "<a href=\"?no=htmlspecialchars($prev_list)&field=htmlspecialchars($field)&search_word=htmlspecialchars($search_word)\">◀</a>\n";
+  echo "<a href=\"?no=$prev_list&field=$field&search_word=$search_word\">◀</a>\n";
   }
 
   for ($i=$start_page;$i <= $end_page;$i++) {
@@ -137,7 +135,7 @@ $current_page = floor($no / $page_size);
     $page_num = $i+1;
 
     if ($no != $page){ //현재 페이지가 아닐 경우만 링크를 표시
-      echo "<a href=\"?no=htmlspecialchars($page)&field=htmlspecialchars($field)&search_word=htmlspecialchars($search_word)\">";
+      echo "<a href=\"?no=$page&field=$field&search_word=$search_word\">";
     }
 
     echo " $page_num ";
@@ -149,7 +147,7 @@ $current_page = floor($no / $page_size);
 
   if($total_page > $end_page) {
   $next_list = ($end_page + 1)* $page_size;
-  echo "<a href=?no=htmlspecialchars($next_list)&field=htmlspecialchars($field)&search_word=htmlspecialchars($search_word)>▶</a><p>";
+  echo "<a href=?no=$next_list&field=$field&search_word=$search_word>▶</a><p>";
   }
   ?>
   </font>
